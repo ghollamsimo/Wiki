482f4f1396084fea4563e108ef715279ecb458b6
ghollamsimo <ghollamsimo1@gmail.com>
2024-01-06 23:22:24 +0100

    Create Structure MVC And Create sql Folder

---
+++ app/config/connection.php
@@ -0,0 +1,23 @@
+<?php
+
+class DataBaseConnection{
+    private $host = 'localhost:3308';
+    private $username = 'root';
+    private $password = '';
+    private $database = 'wiki';
+    private $conn;
+
+    public function __construct(){
+        try {
+            $dbh = "mysql:host={$this->host};dbname={$this->database}";
+            $this->conn = new PDO($dbh, $this->username, $this->password);
+        }catch(PDOException $e){
+            echo 'lahama 9ad La Connection m3a Dik DataBase Lah Irhem Lik lwalidin' . $e->getMessage();
+        }
+    }
+
+    public function prepare($sql)
+    {
+        return $this->conn->prepare($sql);
+    }
+}
